9fa30b4806fd716e4f7fdbff7cac16036d6c6312
tomymonzon_08 <tomymonzon08@gmail.com>
2026-06-23 11:42:28 -0300

    Ejecucion ordenada

--- script.sql
+++ script.sql
@@ -1,27 +1,15 @@
-CREATE TABLE "Estado_prestamo" (
-	"id"	INTEGER NOT NULL,
-	"descripcion"	TEXT NOT NULL,
-	PRIMARY KEY("id")
-);
-
-CREATE TABLE "Estado_reserva" (
-	"id"	INTEGER NOT NULL,
-	"descripcion"	text NOT NULL,
-	PRIMARY KEY("id")
-);
-
-CREATE TABLE "Generos" (
-	"genero_id"	INTEGER NOT NULL,
-	"nombre"	text NOT NULL,
-	PRIMARY KEY("genero_id")
-);
-
 CREATE TABLE "Libro" (
 	"ISBN"	text NOT NULL,
 	"Titulo"	text NOT NULL,
 	"Autor"	TEXT NOT NULL,
 	"CantCopias"	INTEGER NOT NULL,
 	PRIMARY KEY("ISBN")
+);
+
+CREATE TABLE "Generos" (
+	"genero_id"	INTEGER NOT NULL,
+	"nombre"	text NOT NULL,
+	PRIMARY KEY("genero_id")
 );
 
 CREATE TABLE "Libros_Generos" (
@@ -32,11 +20,43 @@ CREATE TABLE "Libros_Generos" (
 	FOREIGN KEY("libro_id") REFERENCES "Libro"("ISBN") ON DELETE CASCADE
 );
 
+CREATE TABLE "Socio" (
+	"NroSocio"	INTEGER NOT NULL,
+	"Nombre"	text NOT NULL,
+	"Apellido"	text NOT NULL,
+	"Email"	text NOT NULL,
+	"tipoSocio"	INTEGER NOT NULL,
+	"activo"	INTEGER NOT NULL,
+	PRIMARY KEY("NroSocio"),
+	FOREIGN KEY("tipoSocio") REFERENCES "TipoSocio"("id") ON DELETE CASCADE
+);
+
+CREATE TABLE "TipoSocio" (
+	"id"	INTEGER NOT NULL,
+	"descripcion"	text NOT NULL,
+	"MaxLibrosSimutaneos"	INTEGER NOT NULL,
+	"DiasPrestamo"	INTEGER NOT NULL,
+	"MultaPorDia"	INTEGER NOT NULL,
+	PRIMARY KEY("id")
+);
+
 CREATE TABLE "Multas" (
 	"Socio_id"	INTEGER NOT NULL,
 	"valor"	INTEGER NOT NULL,
 	PRIMARY KEY("Socio_id"),
 	FOREIGN KEY("Socio_id") REFERENCES "Socio"("NroSocio") ON DELETE CASCADE
+);
+
+CREATE TABLE "Estado_prestamo" (
+	"id"	INTEGER NOT NULL,
+	"descripcion"	TEXT NOT NULL,
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE "Estado_reserva" (
+	"id"	INTEGER NOT NULL,
+	"descripcion"	text NOT NULL,
+	PRIMARY KEY("id")
 );
 
 CREATE TABLE "Prestamo" (
@@ -61,24 +81,4 @@ CREATE TABLE "Reserva" (
 	FOREIGN KEY("Estado") REFERENCES "Estado_reserva"("id") ON DELETE CASCADE,
 	FOREIGN KEY("Libro_id") REFERENCES "Libro"("ISBN") ON DELETE CASCADE,
 	FOREIGN KEY("Socio_id") REFERENCES "Socio"("NroSocio") ON DELETE CASCADE
-);
-
-CREATE TABLE "Socio" (
-	"NroSocio"	INTEGER NOT NULL,
-	"Nombre"	text NOT NULL,
-	"Apellido"	text NOT NULL,
-	"Email"	text NOT NULL,
-	"tipoSocio"	INTEGER NOT NULL,
-	"activo"	INTEGER NOT NULL,
-	PRIMARY KEY("NroSocio"),
-	FOREIGN KEY("tipoSocio") REFERENCES "TipoSocio"("id") ON DELETE CASCADE
-);
-
-CREATE TABLE "TipoSocio" (
-	"id"	INTEGER NOT NULL,
-	"descripcion"	text NOT NULL,
-	"MaxLibrosSimutaneos"	INTEGER NOT NULL,
-	"DiasPrestamo"	INTEGER NOT NULL,
-	"MultaPorDia"	INTEGER NOT NULL,
-	PRIMARY KEY("id")
 );
